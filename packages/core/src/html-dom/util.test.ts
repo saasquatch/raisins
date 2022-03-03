88cbe6deb5f813bbd8c749eddb8a349369cf73b3
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2022-03-03 09:29:39 -0800

    KC: utils visit test for skip node

--- packages/core/src/html-dom/util.test.ts
+++ packages/core/src/html-dom/util.test.ts
@@ -250,3 +250,32 @@ test("Visitor recursive", () => {
   expect(spyRoot).toBeCalledTimes(2);
   expect(spyTag).toBeCalledTimes(5);
 });
+
+test("Visitor skip node", () => {
+  const node: RaisinNode = {
+    type: "root",
+    children: [
+      {
+        type: "tag",
+        tagName: "div",
+        children: [
+          {
+            type: "text",
+            data: "hello world"
+          }
+        ],
+        attribs: {}
+      }
+    ]
+  };
+  const visitor = {
+    skipNode: (_skipNode: any) => false
+  };
+
+  const spySkip = jest.spyOn(visitor, "skipNode");
+
+  visit(node, visitor, true);
+  visit(node, visitor, false);
+
+  expect(spySkip).toBeCalledTimes(2);
+});
