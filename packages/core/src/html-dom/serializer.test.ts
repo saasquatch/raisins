1700d4de26f2902a3ed090f8e6a7e68103c9694c
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2022-03-17 11:59:41 -0700

    KC: serializer + parser tests

--- packages/core/src/html-dom/serializer.test.ts
+++ packages/core/src/html-dom/serializer.test.ts
@@ -90,71 +90,186 @@ describe("Parse + serialize", () => {
     const files = glob.sync(process.cwd() + "/benchmark-files/**/*.html", {
       absolute: true
     });
-
     expect(files?.length).toBeGreaterThan(0);
     for (const file of files) {
-      test(`File: ${file}`, async () => {
-        const source = await fs.readFile(file, "utf-8");
-
-        const node = parse(source, { cleanWhitespace: false });
-        const out = serializer(node);
-
-        const comparisonNode = parse5.parse(source);
-        // const comparisonHTML = parse5serialize(comparisonNode);
-
-        /*
-          Matching on exact HTML is hard.
-           - attributes can use different quotes 'attr' vs "attr"
-           - trailing slashes are optiona <img> vs <img />
-           - whitepace in tags <li> vs <li >
-           - order of attributes <li class="a" id="a"> vs <li id="a" class="a">
-           - equivalent attributes <li attr=""> vs <li attr>
-           
-          Instead we confirm that we have the same number of imporant characters.
-
-          Alternatively, need to use an HTML formatter that can correct for the above errors.
-        */
-        const importantCharsIn = (source.match("<>") || []).length;
-        const importantCharsOut = (out.match("<>") || []).length;
-
-        expect(importantCharsOut).toBe(importantCharsIn);
-
-        /*
-          This second pass helps to make sure that we're not doing anything
-          that produces downstream errors.
-        */
-        serializer(parse(out, { cleanWhitespace: false }));
-
-        const comparisonOutputNode = parse5.parse(out);
-        // const comparisonOutputHtml = parse5serialize(comparisonOutputNode);
-
-        /**
-         * A round-trip through Raisins + parse5
-         * should match a round trip through just parse5
-         */
-        // expect(comparisonOutputHtml).toEqual(comparisonHTML);
-        expect(comparisonOutputNode).toStrictEqual(comparisonNode);
-
-        // TODO: An element with style=";" will produce style="" in first run, and no style attribute in second pass
-        // expect(out2).toEqual(out);
-
-        // TODO: Could use Playwright visual comparisons to confirm HTML looks the same: https://playwright.dev/docs/test-snapshots
+      test(`File ${files.indexOf(file)}: ${file}`, async () => {
+        var source = await fs.readFile(file, "utf-8");
+        testRaisinOutputWithParse5(source);
       });
     }
   });
 });
 
+function testRaisinOutputWithParse5(html: string) {
+  /**
+   * An initial round-trip through parse5 to ensure html
+   * edge cases are not handled differently through two
+   * different parsers
+   */
+  html = parse5.serialize(parse5.parse(html));
 
-function compare(expected:parse5.Document, actual:parse5.Document){
-  const pass = expected.childNodes.every((value,index)=>{
-    return compareNode(value, actual.childNodes[index])
+  /**
+   * Strip all styles as parse5 and Raisins treat styles
+   * differently.
+   *
+   * Raisins uses css-om to parse and validate CSS while
+   * parse5 keeps the exact string.
+   */
+  html = cleanUpCSS(html);
+
+  const raisinNode = parse(html, { cleanWhitespace: false });
+  const raisinString = serializer(raisinNode);
+
+  /**
+   * A round-trip through Raisins + parse5
+   * should match a round trip through just parse5
+   */
+
+  const parsedOriginal = parse5.parse(html);
+  const parsedRaisin = parse5.parse(raisinString);
+
+  purify(parsedOriginal);
+  purify(parsedRaisin);
+
+  // fs.writeFileSync("parsedSource.html", html);
+  // fs.writeFileSync("parsedOriginal.html", parse5.serialize(parsedOriginal));
+  // fs.writeFileSync("parsedRaisin.html", parse5.serialize(parsedRaisin));
+
+  expect(parsedRaisin).toStrictEqual(parsedOriginal);
+
+  // TODO: Could use Playwright visual comparisons to confirm HTML looks the same: https://playwright.dev/docs/test-snapshots
+
+  /*
+    Matching on exact HTML is hard.
+    - attributes can use different quotes 'attr' vs "attr"
+    - trailing slashes are optiona <img> vs <img />
+    - whitepace in tags <li> vs <li >
+    - order of attributes <li class="a" id="a"> vs <li id="a" class="a">
+    - equivalent attributes <li attr=""> vs <li attr>
+
+    Instead we confirm that we have the same number of imporant characters.
+  */
+  const importantCharsIn = (
+    parse5.serialize(parsedOriginal).match(/<|>/g) || []
+  ).length;
+  const importantCharsOut = (parse5.serialize(parsedRaisin).match(/<|>/g) || [])
+    .length;
+
+  expect(importantCharsOut).toBe(importantCharsIn);
+}
+
+/*
+  Absolves parse5 nodes from their sins by:
+    - removing parentNode circular references
+    - sorting attributes to be in the same order (if they exist)
+    - removing text nodes caused by whitespaces between elements
+      or containing html elements
+*/
+function purify(...nodes: any[]) {
+  nodes.map(node => {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      var child = node.childNodes[i];
+      if (child.hasOwnProperty("attrs") && child.attrs.length > 0) {
+        child.attrs.sort(attributeCompare);
+      }
+      if (child.hasOwnProperty("parentNode")) {
+        delete child["parentNode"];
+      }
+      if (child.nodeName === "#text") {
+        if (!/\S/.test(child.value) || /\<|&..|\>/.test(child.value)) {
+          node.childNodes.splice(i, 1);
+          i--;
+        }
+      }
+      if (child.hasOwnProperty("childNodes")) {
+        purify(child);
+      }
+    }
   });
-  if(!pass){
-    throw new Error("");
+}
+
+/*
+  Sort function for comparing two nodes by name
+*/
+function attributeCompare(a: any, b: any) {
+  if (a.name < b.name) {
+    return -1;
   }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
 }
 
-function compareNode(expected:parse5.ChildNode, actual:parse5.ChildNode){
+/**
+ * Strips all styles from html
+ */
+function cleanUpCSS(html: string) {
+  html = html
+    .replace(/<\s*style.*?<\W+style\s*>/gs, "<style>CSSPARSE</style>") // replaces the content between <style> tags
+    .replace(/style=([\"'])(?:(?=(\\?))\2.)*?\1/gs, 'style="CSSPARSE"'); // replaces the content inside style attribs
 
-  expected.
+  // TODO: Could use a third party CSS parser instead of regex
+
+  return html;
 }
+
+describe("Parse comparison functions", () => {
+  test("Attributes with different orders", () => {
+    const node_a = parse5.parse("<div left center right>I am a div</div>");
+    const node_b = parse5.parse("<div center right left >I am a div</div>");
+    expect(node_a).toStrictEqual(node_b);
+
+    purify(node_a, node_b);
+    expect(node_a).toStrictEqual(node_b);
+  });
+
+  test("Inline styles with different syntax", () => {
+    var style_a = `<div style="display: none;">I am a div</div>`;
+    var style_b = `<div style='display:none'>I am a div</div>`;
+    expect(parse5.parse(style_a)).not.toStrictEqual(parse5.parse(style_b));
+
+    style_a = cleanUpCSS(style_a);
+    style_b = cleanUpCSS(style_b);
+    expect(parse5.parse(style_a)).toStrictEqual(parse5.parse(style_b));
+  });
+
+  test("Styles tags <style> with different syntax", () => {
+    var style_a = `<style>display: none;</style>`;
+    var style_b = `<style>  display: none</style>`;
+    expect(parse5.parse(style_a)).not.toStrictEqual(parse5.parse(style_b));
+
+    style_a = cleanUpCSS(style_a);
+    style_b = cleanUpCSS(style_b);
+    expect(parse5.parse(style_a)).toStrictEqual(parse5.parse(style_b));
+  });
+
+  test("Different whitespaces between HTML elements", () => {
+    const html_a = `
+    <!DOCTYPE html>
+    <html>
+
+    <body>
+        <h1>My First Heading</h1>
+        <p>My first paragraph.</p>
+    </body>
+
+    </html>
+   `;
+    const html_b = `   
+   <!DOCTYPE html>
+        <html>
+              <body>
+                  <h1>My First Heading</h1>
+                  <p>My first paragraph.</p>
+              </body>          </html>
+  `;
+
+    const node_a = parse5.parse(html_a);
+    const node_b = parse5.parse(html_b);
+    expect(node_a).not.toStrictEqual(node_b);
+
+    purify(node_a, node_b);
+    expect(node_a).toStrictEqual(node_b);
+  });
+});
