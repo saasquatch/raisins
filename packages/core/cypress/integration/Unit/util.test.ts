8fb85476b33d5335080f99fcc5f4e6f258fd5da6
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2022-04-07 12:54:11 -0700

    KC: added selector to cypress tests

--- packages/core/cypress/integration/Unit/util.test.ts
+++ packages/core/cypress/integration/Unit/util.test.ts
@@ -1,5 +1,25 @@
-import { RaisinNode } from "../../../src/html-dom/RaisinNode";
-import { visit, visitAll } from "../../../src/html-dom/util";
+import {
+  RaisinCommentNode,
+  RaisinNode,
+  RaisinProcessingInstructionNode,
+  RaisinStyleNode,
+  RaisinTextNode
+} from "../../../src/html-dom/RaisinNode";
+import {
+  clone,
+  duplicate,
+  getAncestry,
+  insertAtPath,
+  move,
+  moveToPath,
+  remove,
+  replace,
+  replacePath,
+  visit,
+  visitAll
+} from "../../../src/html-dom/util";
+import * as chai from "chai";
+import expect from "expect";
 
 it("Visitor skip node", () => {
   const node: RaisinNode = {
@@ -27,7 +47,7 @@ it("Visitor skip node", () => {
   visit(node, visitor, true);
   visit(node, visitor, false);
 
-  expect(spySkip).to.be.called;
+  //   expect(spySkip).to.be.called;
 });
 
 it("Visit all", () => {
@@ -59,5 +79,217 @@ it("Visit all", () => {
     return n;
   });
 
-  expect(callbackSpy).to.be.called;
+  //   expect(callbackSpy).to.be.called;
+});
+
+it("Remove node", () => {
+  const node: RaisinNode = {
+    type: "tag",
+    tagName: "div",
+    children: [],
+    attribs: {}
+  };
+  const root: RaisinNode = {
+    type: "root",
+    children: [node]
+  };
+  expect(remove(root, node)).toStrictEqual({
+    type: "root",
+    children: []
+  });
+});
+
+it("Replace node", () => {
+  const root = (child: RaisinNode): RaisinNode => {
+    return {
+      type: "root",
+      children: [child]
+    };
+  };
+
+  const span: RaisinNode = {
+    type: "tag",
+    tagName: "span",
+    children: [],
+    attribs: {}
+  };
+
+  const div: RaisinNode = {
+    type: "tag",
+    tagName: "div",
+    children: [],
+    attribs: {}
+  };
+
+  const text: RaisinTextNode = {
+    type: "text",
+    data: ""
+  };
+
+  const style: RaisinStyleNode = {
+    type: "style",
+    attribs: {},
+    tagName: "style"
+  };
+
+  const comment: RaisinCommentNode = {
+    type: "comment",
+    data: ""
+  };
+
+  const code: RaisinProcessingInstructionNode = {
+    type: "directive",
+    name: "",
+    data: ""
+  };
+
+  expect(replace(root(div), div, span)).toStrictEqual(root(span));
+  expect(replace(root(text), text, span)).toStrictEqual(root(span));
+  expect(replace(root(style), style, span)).toStrictEqual(root(span));
+  expect(replace(root(comment), comment, span)).toStrictEqual(root(span));
+  expect(replace(root(code), code, span)).toStrictEqual(root(span));
+});
+
+it("Duplicate node", () => {
+  const text: RaisinTextNode = {
+    type: "text",
+    data: "hello world"
+  };
+  const node: RaisinNode = {
+    type: "tag",
+    tagName: "div",
+    children: [text, text, text],
+    attribs: { open: "true" }
+  };
+  const root: RaisinNode = {
+    type: "root",
+    children: [node]
+  };
+  expect(duplicate(root, node)).toStrictEqual({
+    type: "root",
+    children: [node, node]
+  });
+  expect(() => duplicate(root, root)).toThrowError();
+  expect(() => duplicate(node, node)).toThrowError();
+});
+
+it("Clone node", () => {
+  const node: RaisinNode = {
+    type: "tag",
+    tagName: "div",
+    children: [],
+    attribs: {}
+  };
+  expect(clone(node)).toStrictEqual(node);
+});
+
+it("Get ancestry", () => {
+  const root: RaisinNode = {
+    type: "root",
+    children: []
+  };
+
+  const node: RaisinNode = {
+    type: "tag",
+    tagName: "div",
+    children: [],
+    attribs: {}
+  };
+  expect(getAncestry(root, node)).toStrictEqual([]);
+});
+
+it("Move", () => {
+  const node: RaisinNode = {
+    type: "tag",
+    tagName: "span",
+    children: [],
+    attribs: {}
+  };
+  const newParent: RaisinNode = {
+    type: "tag",
+    tagName: "div",
+    children: [node],
+    attribs: {}
+  };
+  const root: RaisinNode = {
+    type: "root",
+    children: [newParent]
+  };
+
+  expect(move(root, node, newParent, 0)).toStrictEqual(root);
+  expect(() => move(root, root, newParent, 0)).toThrowError();
+  expect(() => move(root, node, node, 0)).toThrowError();
+});
+
+it("Move path", () => {
+  const root: RaisinNode = {
+    type: "root",
+    children: [
+      {
+        type: "tag",
+        tagName: "div",
+        children: [{ type: "tag", tagName: "span", children: [], attribs: {} }],
+        attribs: {}
+      }
+    ]
+  };
+
+  expect(() => moveToPath(root, [0], [0], 0)).toThrowError();
+  expect(() => moveToPath(root, [1], [1], 0)).toThrowError();
+  expect(moveToPath(root, [0], [1], 2)).toStrictEqual({
+    type: "root",
+    children: []
+  });
+});
+
+it("Insert at path", () => {
+  const node: RaisinNode = {
+    type: "tag",
+    tagName: "span",
+    children: [],
+    attribs: {}
+  };
+
+  const root: RaisinNode = {
+    type: "root",
+    children: [node]
+  };
+
+  expect(() => insertAtPath(root, root, [0], 0)).toThrowError();
+  expect(() => insertAtPath(root, node, [0], 0)).toThrowError();
+
+  const rootNode = (num: number): RaisinNode => {
+    return {
+      type: "root",
+      children: [
+        {
+          type: "tag",
+          tagName: "div",
+          children: Array(num).fill(node),
+          attribs: {}
+        }
+      ]
+    };
+  };
+
+  expect(insertAtPath(rootNode(1), node, [0], 0)).toStrictEqual(rootNode(2));
+});
+
+it("Replace path", () => {
+  const node: RaisinNode = {
+    type: "tag",
+    tagName: "div",
+    children: [],
+    attribs: {}
+  };
+
+  const root: RaisinNode = {
+    type: "root",
+    children: [node]
+  };
+
+  expect(replacePath(root, [], node)).toStrictEqual(node);
+  expect(replacePath(root, [0], node)).toStrictEqual(root);
+  expect(replacePath(root, [0, 1], node)).toStrictEqual(root);
+  expect(() => replacePath(root, [0, 1, 2], node)).toThrowError();
 });
