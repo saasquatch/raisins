dce5540ac6eac8f5fd1abdd3bd4014731e74a52e
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2022-04-18 17:02:56 -0700

    KC: cleanup explorative test

--- packages/core/src/validation/validateNode/validationAttrib.iso.test.ts
+++ packages/core/src/validation/validateNode/validationAttrib.iso.test.ts
@@ -1,8 +1,6 @@
 import { CustomElement } from "@raisins/schema/schema";
 import { RaisinElementNode } from "../../html-dom/RaisinNode";
-import { isValidColor, isValidURL, validateAttributes } from "./validateNode";
-import validateColor from "validate-color";
-import { parseToRgba } from "color2k";
+import { validateAttributes } from "./validateNode";
 
 describe("", () => {
   it("", () => {
@@ -34,55 +32,6 @@ describe("", () => {
       children: []
     };
 
-    // COLOR TEST
-    const color = "inherit";
-    console.log("validateColor:", color, validateColor(color));
-    console.log("isValidColor:", color, isValidColor(color));
-    console.log("\n");
-
-    var date;
-
-    date = "2020-07-07T12:00:00+08:00/2021-08-08T12:00:00+08:00";
-    date = "2020-07-07T12:00:00+08:00/2021-08-08T12:00:00+08:00";
-    // date = "2021-12-31/2021-12-31";
-    // Check with Scott leaderboard interval filter
-
-    console.log("isValidDateInterval", isValidDateInterval(date));
-    console.log("\n");
-
-    var url;
-    // url = "https://www.digi.com/resources/documentation/digidocs/90001437-13/reference/r_iso_8601_duration_format.htm";
-    url = "www.asd.com";
-    console.log("isValidURL", isValidURL(url));
-
-    function isValidDateInterval(value: string): boolean {
-      const dates = value.split("/");
-      // if there are not exactly 2 dates, it is not a valid interval
-      if (dates.length !== 2) return false;
-      // if both are valid dates, then it is a valid date interval
-      if (isValidDate(dates[0]) && isValidDate(dates[1])) return true;
-      return false;
-    }
-
-    /*
-	const whitelist = ["currentColor", "var(--sl-color-sunshine-500)"];
-	// CSS variable support inherit/currentColor
-	// var(--sl-***-**)
-    if (whitelist.includes(value)) return true;
-	*/
-
-    // test and specs
-
-    function isValidDate(value: string): boolean {
-      try {
-        new URL(value);
-      } catch {
-        return false;
-      }
-      return true;
-    }
-
-    console.log(validateAttributes(node, [meta]));
-    // console.log(validateAttributes(node, [meta])[0]?.error || "no errors :)");
+    console.log(validateAttributes(node, [meta])[0]?.error || "no errors :)");
   });
 });
