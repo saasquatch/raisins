109650cb1984d24e9e11f59569f4bde9b39ba913
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2022-04-20 14:19:21 -0700

    Adding more story examples in selected node, testing out an attribute controller with groups

--- packages/react/src/core/selection/SelectedNodeController.stories.tsx
+++ packages/react/src/core/selection/SelectedNodeController.stories.tsx
@@ -1,13 +1,20 @@
-import { atom } from 'jotai';
-import { molecule } from 'jotai-molecules';
+import { atom, useAtomValue } from 'jotai';
+import { molecule, useMolecule } from 'jotai-molecules';
 import React from 'react';
 import { RaisinConfig } from '..';
-import { AttributesController } from '../../attributes';
+import {
+  AttributeMolecule,
+  AttributeProvider,
+  AttributesController,
+  AttributesControllerProps,
+  AttributesMolecule,
+} from '../../attributes';
 import { CanvasController } from '../../canvas';
 import {
   mintMono,
   MintComponents,
   mintReferralTable,
+  mintBigStat,
 } from '../../examples/MintComponents';
 import { widgets } from '../../examples/MockWidgets';
 import {
@@ -30,15 +37,31 @@ const ConfigMolecule = molecule<Partial<RaisinConfig>>((getMol) => {
   };
 });
 
-const AttributeEditor = () => (
+const AttributeEditor = ({
+  Controller = () => (
+    <SelectedNodeController
+      HasSelectionComponent={AttributesController}
+    ></SelectedNodeController>
+  ),
+}) => (
   <div style={{ display: 'flex' }}>
-    <div style={{ width: '50%' }}>
-      <SelectedNodeController
-        HasSelectionComponent={AttributesController}
-        NoSelectionComponent={() => <div>no selection</div>}
-      ></SelectedNodeController>
+    <div
+      style={{
+        width: '50%',
+        position: 'fixed',
+        overflowY: 'scroll',
+        height: '95vh',
+      }}
+    >
+      <Controller />
     </div>
-    <div style={{ width: '50%' }}>
+    <div
+      style={{
+        width: '50%',
+        position: 'absolute',
+        right: 0,
+      }}
+    >
       <CanvasController />
     </div>
   </div>
@@ -68,6 +91,18 @@ export const MintReferralTable = () => {
   );
 };
 
+export const MintBigStat = () => {
+  return (
+    <BasicStory
+      startingHtml={mintBigStat}
+      startingPackages={MintComponents}
+      Molecule={ConfigMolecule}
+    >
+      <AttributeEditor />
+    </BasicStory>
+  );
+};
+
 export const Vanilla = () => {
   return (
     <BasicStory
@@ -78,4 +113,103 @@ export const Vanilla = () => {
       <AttributeEditor />
     </BasicStory>
   );
+};
+
+export const CustomUnselectedComponent = () => {
+  return (
+    <BasicStory
+      startingHtml={mintMono}
+      startingPackages={MintComponents}
+      Molecule={ConfigMolecule}
+    >
+      <AttributeEditor
+        Controller={() => (
+          <SelectedNodeController
+            HasSelectionComponent={AttributesController}
+            NoSelectionComponent={() => (
+              <div>
+                <h3>No component currently selected.</h3>
+                <p>Select a component to begin customizing!</p>
+              </div>
+            )}
+          ></SelectedNodeController>
+        )}
+      />
+    </BasicStory>
+  );
+};
+
+export const CustomSelectedComponentMint = () => {
+  return (
+    <BasicStory
+      startingHtml={mintMono}
+      startingPackages={MintComponents}
+      Molecule={ConfigMolecule}
+    >
+      <AttributeEditor
+        Controller={() => (
+          <SelectedNodeController
+            HasSelectionComponent={CustomAttributesController}
+          ></SelectedNodeController>
+        )}
+      />
+    </BasicStory>
+  );
+};
+
+export const CustomSelectedComponentVanilla = () => {
+  return (
+    <BasicStory
+      startingHtml={referrerWidget}
+      startingPackages={VanillaComponents}
+      Molecule={ConfigMolecule}
+    >
+      <AttributeEditor
+        Controller={() => (
+          <SelectedNodeController
+            HasSelectionComponent={CustomAttributesController}
+          ></SelectedNodeController>
+        )}
+      />
+    </BasicStory>
+  );
+};
+
+const CustomAttributesController: React.FC<AttributesControllerProps> = (
+  props
+) => {
+  const { keysAtom, groupedSchemaAtom } = useMolecule(AttributesMolecule);
+  const keys = useAtomValue(keysAtom);
+  const groupedSchema = useAtomValue(groupedSchemaAtom);
+
+  if (!keys) return <></>;
+  const Component = props.Component ?? DefaultAttributeComponent;
+  return (
+    <>
+      {Object.keys(groupedSchema).map((key) => {
+        return (
+          <details>
+            <summary>{key}</summary>
+            {groupedSchema[key].map((attribute) => {
+              return (
+                <AttributeProvider
+                  attributeName={attribute.name}
+                  key={attribute.name}
+                >
+                  <Component />
+                </AttributeProvider>
+              );
+            })}
+            <hr />
+          </details>
+        );
+      })}
+    </>
+  );
+};
+
+const DefaultAttributeComponent = () => {
+  const { FieldAtom } = useMolecule(AttributeMolecule);
+  const Field = useAtomValue(FieldAtom);
+  return <Field />;
 };
