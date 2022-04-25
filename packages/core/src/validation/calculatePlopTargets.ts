b8200af061431bcc14d9ba7103b26378a44b899f
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2022-04-25 15:22:45 -0700

    KC: calculateploptargets

--- packages/core/src/validation/calculatePlopTargets.ts
+++ packages/core/src/validation/calculatePlopTargets.ts
@@ -1,4 +1,5 @@
 import { CustomElement } from "@raisins/schema/schema";
+import _ from "lodash";
 import { isElementNode } from "../html-dom/isNode";
 import { RaisinNode } from "../html-dom/RaisinNode";
 import { isNodeAllowed } from "./rules/isNodeAllowed";
@@ -15,52 +16,147 @@ export function calculatePlopTargets(
     parentMeta: CustomElement;
     possiblePlopMeta: CustomElement;
   }
-): PlopTarget[] {
-  // TODO: Root node should allow any children
+): PlopTarget[] | any {
+  // deal with root
   if (!isElementNode(parent)) return [];
-  const raisinChildren = parent.children;
 
-  if (parent.children.length <= 0) {
-    // Slot plop targets when there are no children
-    return (
-      schema.parentMeta.slots?.map((s, idx) => {
-        return {
-          slot: s.name,
-          idx,
-          parent
-        };
-      }) ?? []
-    );
-  }
-  const newChildren =
-    raisinChildren?.reduce((acc, raisinChild, idx) => {
-      if (
-        // No plop around text nodes
-        !isElementNode(raisinChild) ||
-        // No plops around picked node (that's redundant)
-        raisinChild === possiblePlop
-      ) {
-        // No plop targets around element nodes
-        return acc;
-      }
-      const slot = raisinChild.attribs.slot ?? "";
-      const isValid = isNodeAllowed(
-        possiblePlop,
-        schema.possiblePlopMeta,
-        parent,
-        schema.parentMeta,
-        slot
-      );
-      if (!isValid) return acc;
+  if (schema.parentMeta.slots === undefined) return [];
 
-      const plopBefore = {
-        idx,
-        slot,
+  const slots = schema.parentMeta.slots.map(slot => slot.name);
+  const slotList = slots.reduce((obj, value) => ({ ...obj, [value]: [] }), {});
+
+  parent.children.forEach(child => {
+    if (!isElementNode(child)) return;
+
+    if (Object.hasOwn(slotList, child.attribs.slot)) {
+      //@ts-ignore
+      slotList[child.attribs.slot].push(child);
+    }
+  });
+
+  const targets: PlopTarget[] = [];
+
+  let idx = 0;
+  slots.forEach(slot => {
+    //@ts-ignore
+    if (slotList[slot].length === 0)
+      targets.push({
+        slot: slot,
+        idx: idx,
         parent
-      };
-      return [...acc, plopBefore];
-    }, [] as PlopTarget[]) ?? [];
+      });
+    //@ts-ignore
+    slotList[slot].forEach(element => {
+      if (element) {
+        targets.push({
+          slot: slot,
+          idx: idx,
+          parent
+        });
 
-  return newChildren;
-  return [];
+        targets.push({
+          slot: slot,
+          idx: idx + 1,
+          parent
+        });
+
+        idx = idx + 1;
+      } else {
+        targets.push({
+          slot: slot,
+          idx: idx,
+          parent
+        });
+      }
+    });
+  });
+
+  //@ts-ignore
+  const plopSlots = schema.possiblePlopMeta.slots.map(slot => slot.name) ?? [];
+
+  return targets
+    .filter(
+      (v, i, a) =>
+        a.findIndex(t => JSON.stringify(t) === JSON.stringify(v)) === i
+    )
+    .filter(n => plopSlots.includes(n.slot));
+
+  // // TODO: Root node should allow any children
+  // if (!isElementNode(parent)) return [];
+  // const raisinChildren = parent.children;
+  // const plopSlot = schema.possiblePlopMeta.slots?.map(e => e.name) ?? [];
+  // const seenSlots = new Set(plopSlot);
+
+  // if (parent.children.length <= 0) {
+  //   // Slot plop targets when there are no children
+  //   return (
+  //     schema.parentMeta.slots
+  //       ?.filter(value => plopSlot.includes(value.name))
+  //       .map((s, idx) => {
+  //         return {
+  //           slot: s.name,
+  //           idx: 0,
+  //           parent
+  //         };
+  //       }) ?? []
+  //   );
+  // }
+
+  // var lastIdx = 0;
+
+  // const newChildren =
+  //   raisinChildren?.reduce((acc, raisinChild, idx) => {
+  //     if (
+  //       // No plop around text nodes
+  //       !isElementNode(raisinChild) ||
+  //       // No plops around picked node (that's redundant)
+  //       raisinChild === possiblePlop
+  //     ) {
+  //       // No plop targets around element nodes
+  //       return acc;
+  //     }
+
+  //     const slot = raisinChild.attribs.slot ?? "";
+  //     const isValid = isNodeAllowed(
+  //       possiblePlop,
+  //       schema.possiblePlopMeta,
+  //       parent,
+  //       schema.parentMeta,
+  //       slot
+  //     );
+  //     if (!isValid) return acc;
+
+  //     if (!plopSlot.includes(slot)) return acc;
+  //     else seenSlots.delete(slot);
+
+  //     const plopBefore = {
+  //       idx,
+  //       slot,
+  //       parent
+  //     };
+
+  //     const plopAfter = {
+  //       idx: idx + 1,
+  //       slot,
+  //       parent
+  //     };
+
+  //     lastIdx = idx + 1;
+  //     return [...acc, plopBefore, plopAfter];
+  //   }, [] as PlopTarget[]) ?? [];
+
+  // console.log("seen", seenSlots);
+  // console.log(newChildren);
+  // seenSlots.forEach(slot => {
+  //   console.log(slot);
+  //   newChildren.push({
+  //     idx: lastIdx,
+  //     slot,
+  //     parent
+  //   });
+  // });
+
+  // return newChildren.filter(
+  //   (v, i, a) => a.findIndex(t => JSON.stringify(t) === JSON.stringify(v)) === i
+  // );
 }
--- packages/core/src/validation/calculatePropTargets.iso.test.ts
+++ packages/core/src/validation/calculatePropTargets.iso.test.ts
@@ -1,25 +1,113 @@
 import parse from "../html-dom/parser";
 import { CustomElement } from "@raisins/schema/schema";
 import { calculatePlopTargets } from "./calculatePlopTargets";
+import { RaisinElementNode } from "../html-dom/RaisinNode";
+import expect from "expect";
 
 describe("", () => {
-  it("", () => {
-    const parent = parse("<div></div>");
-    const possiblePlop = parse("<span></span>");
+  //   it("Calculate Plop Targets - Single slot", () => {
+  //     const node: RaisinElementNode = {
+  //       type: "tag",
+  //       attribs: { slot: "children" },
+  //       tagName: "sqm-timeline-stuff",
+  //       children: []
+  //     };
+
+  //     const plop: RaisinElementNode = {
+  //       type: "tag",
+  //       attribs: { slot: "children" },
+  //       tagName: "sqm-timeline-entry",
+  //       children: []
+  //     };
+  //     const plopMeta: CustomElement = {
+  //       tagName: "sqm-timeline-entry",
+  //       slots: [{ name: "chisldren" }]
+  //     };
+
+  //     const parentEmpty: RaisinElementNode = {
+  //       type: "tag",
+  //       attribs: {},
+  //       tagName: "sqm-timeline",
+  //       children: []
+  //     };
+
+  //     const parent1Node: RaisinElementNode = {
+  //       type: "tag",
+  //       attribs: {},
+  //       tagName: "sqm-timeline",
+  //       children: [node]
+  //     };
+
+  //     const parent2Node: RaisinElementNode = {
+  //       type: "tag",
+  //       attribs: {},
+  //       tagName: "sqm-timeline",
+  //       children: [node, node]
+  //     };
+
+  //     const parent3Node: RaisinElementNode = {
+  //       type: "tag",
+  //       attribs: {},
+  //       tagName: "sqm-timeline",
+  //       children: [node, node, node]
+  //     };
+
+  //     const parentMeta: CustomElement = {
+  //       tagName: "sqm-timeline",
+  //       slots: [{ name: "children" }]
+  //     };
+  //     const schema = {
+  //       parentMeta,
+  //       possiblePlopMeta: plopMeta
+  //     };
+
+  //     //   expect(calculatePlopTargets(parentEmpty, plop, schema).length).toBe(1);
+  //     //   expect(calculatePlopTargets(parent1Node, plop, schema).length).toBe(2);
+  //     //   expect(calculatePlopTargets(parent2Node, plop, schema).length).toBe(3);
+  //     //   expect(calculatePlopTargets(parent3Node, plop, schema).length).toBe(4);
+
+  //     //   const res = calculatePlopTargets(parent3Node, plop, schema);
+  //     //   console.log(res);
+  //   });
+
+  it("Calculate Plop Targets - Double slot", () => {
+    const plop: RaisinElementNode = {
+      type: "tag",
+      tagName: "sqm-timeline-entry",
+      attribs: {},
+      children: []
+    };
+    const possiblePlopMeta: CustomElement = {
+      tagName: "sqm-timeline-entry",
+      slots: [{ name: "slot-a" }, { name: "slot-b" }, { name: "slot-c" }]
+    };
+
+    const node = (type: string): RaisinElementNode => {
+      return {
+        type: "tag",
+        tagName: "sqm-timeline-entry",
+        attribs: { slot: type },
+        children: []
+      };
+    };
 
     const parentMeta: CustomElement = {
-		tagName: ""
-	};
-    const possiblePlopMeta: CustomElement = {
-		tagName: ""
-	};
+      tagName: "sqm-timeline",
+      slots: [{ name: "slot-a" }, { name: "slot-b" }, { name: "slot-c" }]
+    };
+
+    const parent1A: RaisinElementNode = {
+      type: "tag",
+      tagName: "sqm-timeline",
+      attribs: {},
+      children: [node('slot-d')]
+    };
+
     const schema = {
-		parentMeta,
-		possiblePlopMeta
-	};
-
-    const res = calculatePlopTargets(parent, possiblePlop, schema);
-
-    console.log(res);
+      parentMeta,
+      possiblePlopMeta
+    };
+    const res = calculatePlopTargets(parent1A, plop, schema);
+    console.log("\n\n", res);
   });
 });
