7ac56af161eed51f64674765ff3972364e5cebac
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2022-04-27 18:44:03 -0700

    another build / publish with link updates

--- packages/react/src/rich-text/index.ts
+++ packages/react/src/rich-text/index.ts
@@ -4,6 +4,7 @@ export {
   ProseEditorScopeProvider,
 } from './prosemirror/ProseEditorScope';
 export { ProseEditorStateMolecule } from './prosemirror/ProseEditorStateMolecule';
+export { ProseToggleMarkMolecule } from './prosemirror/ProseToggleMarkMolecule';
 export { NodeRichTextController } from './RichTextEditor';
 export { SelectedNodeRichTextEditor } from './SelectedNodeRichTextEditor';
 export { RichTextMolecule } from './RichTextMolecule';
