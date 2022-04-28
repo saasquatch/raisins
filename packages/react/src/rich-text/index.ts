e4d97ea159ae289781d8d0e29bd8967a5e95b2ec
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2022-04-27 18:19:04 -0700

    making sure exports get all the way through to portalv2

--- packages/react/src/rich-text/index.ts
+++ packages/react/src/rich-text/index.ts
@@ -1,3 +1,9 @@
-export * from './prosemirror';
+export * from './prosemirror/default-schema';
+export {
+  ProseEditorScope,
+  ProseEditorScopeProvider,
+} from './prosemirror/ProseEditorScope';
+export { ProseEditorStateMolecule } from './prosemirror/ProseEditorStateMolecule';
 export { NodeRichTextController } from './RichTextEditor';
 export { SelectedNodeRichTextEditor } from './SelectedNodeRichTextEditor';
+export { RichTextMolecule } from './RichTextMolecule';
