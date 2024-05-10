772aa42a5ef563dd3958d69f0a9e6882272ec929
Logan Volkers <loganvolkers@users.noreply.github.com>
2024-05-10 17:44:11 +0000

    At linting to schema and docs target

--- packages/schema/schema.d.ts
+++ packages/schema/schema.d.ts
@@ -149,6 +149,7 @@ export interface Attribute {
   /**
    * Array of possible values to select from where elements might be of any type, including null.
    */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   enum?: any[];
 
   /**
