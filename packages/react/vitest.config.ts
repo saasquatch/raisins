ca51d963f7a42ddfe4870413f6f2f1af4268a2d8
Logan Volkers <loganvolkers@users.noreply.github.com>
2024-05-13 18:16:40 +0000

    Run playwright tests

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['node_modules'],
+    exclude: ['node_modules', 'end2end'],
     globals: true,
     environment: 'jsdom',
   },
