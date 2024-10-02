a4e4b325ff5d407f482b0d3a1e406a0d517ec6cb
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2024-10-02 11:05:53 -0700

    fix optic types

--- packages/react/src/node/atoms/atomForChildren.tsx
+++ packages/react/src/node/atoms/atomForChildren.tsx
@@ -1,14 +1,13 @@
 import { RaisinNode } from '@raisins/core';
 import { PrimitiveAtom } from 'jotai';
 import { focusAtom } from 'jotai-optics';
-import { OpticFor } from 'optics-ts';
+import { OpticFor_ } from 'optics-ts';
 
-const childOptic = (o: OpticFor<RaisinNode>) =>
+const childOptic = (o: OpticFor_<RaisinNode>) =>
   o.prop('children' as any).valueOr([]);
 
 export function atomForChildren(
   nodeAtom: PrimitiveAtom<RaisinNode>
 ): PrimitiveAtom<RaisinNode[]> {
-  // @ts-expect-error jotai-optics types have changed
   return focusAtom(nodeAtom, childOptic);
 }
